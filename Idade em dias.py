0ab5e197145c343ced5d66817f2abcfadf454f30
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-03-21 11:12:56 -0300

    modulo de soma

--- Idade em dias.py
+++ Idade em dias.py
@@ -1,4 +1,4 @@
-total_dias = int (input())
+total_dias = int (input(''))
 
 idade_ano=total_dias/365
 total_dias=total_dias%365
@@ -6,6 +6,12 @@ idade_meses=total_dias/30
 total_dias=total_dias%30
 idade_dias=total_dias
 
-print(int(idade_ano),' ano (s)')
-print(int(idade_meses),'mes (es)')
-print(idade_dias, 'dia (s)')
+print(int(idade_ano),'ano(s)')
+print(int(idade_meses),'mes(es)')
+print(int(idade_dias),'dia(s)')
+
+'''
+1 ano(s)
+1 mes(es)
+5 dia(s)
+'''
---
+++ Modulos/images.py
@@ -0,0 +1,28 @@
+#O modulo é uma coleção de funções
+
+tamanho = 1000
+
+def rotaciona90sh(imagem):
+    a=10
+    #codigo
+    #codigo
+    #codigo
+
+def rotaciona90sh(imagem):
+    a = 10
+    #codigo
+    #codigo
+    #codigo
+
+def rotaciona180sh(imagem):
+    rotaciona90sh(imagem)
+    rotaciona90sh(imagem)
+
+if __name__ == "__main__":
+    print("Rodei o Arquivo")
+    #image = leituraimagem()
+
+'''
+else:
+    print("usei o modulo:",__name__)
+    print("usei direito o modulo")'''
