612260294dd0155c5988d3bdc4fea6c3d04326c2
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-07-25 18:07:44 -0300

    arquivo advinhacao

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,17 @@
+print("##################################")
 print("Bem vindos ao jogo da Adivinhação!")
+print("##################################")
+
+numero_secreto = 42
+
+chute = int(input ("Digite o seu número: "))
+
+print("Você digitou", chute)
+
+if (numero_secreto == chute):
+    print("Você acertou")
+    print("Número Secreto: ", numero_secreto)
+else:
+    print("Você errou!!!")
+
+print("\nO Jogo Terminou !!!")
