86a396a1649885749dfa90a6e77b944139275ce0
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-11 10:48:07 -0300

    pasta jogos deletada e movida para outro diretório

---
+++ SMS/sms_test.py
@@ -0,0 +1,3 @@
+import pywhatkit as kit
+pip install pywhatkit
+kit.sendwhatmsg("55 11 980505416","teste",2,15)
--- SMS/venv/Scripts/easy_install-script.py
+++ SMS/venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!C:\git\Codigos-em-Python-3\jogos\venv\Scripts\python.exe
+#!C:\git\Codigos-em-Python-3\SMS\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
