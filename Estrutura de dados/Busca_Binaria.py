8570d6828f55aadb044388a0d808a4e248037d9e
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-18 20:14:03 -0300

    Estrutura de dados alteração

--- Estrutura de dados/Busca_Binaria.py
+++ Estrutura de dados/Busca_Binaria.py
@@ -11,3 +11,17 @@ numeros = [5, 11 ,13 ,33 ,61, 73 ,79 ,80 ,85]
 print(busca_indice(numeros,5))
 print(busca_indice(numeros,85))
 print(busca_indice(numeros,7))
+
+print("-------------------------")
+
+def busca_indice(lista, alvo):
+    for i in range(0, len(lista)):
+        if lista[i] == alvo:
+            return i
+    return -1
+
+# Testando a busca
+numeros = [5, 11 ,13 ,33 ,61, 73 ,79 ,80 ,85]
+print(busca_indice(numeros,5))
+print(busca_indice(numeros,85))
+print(busca_indice(numeros,7))
