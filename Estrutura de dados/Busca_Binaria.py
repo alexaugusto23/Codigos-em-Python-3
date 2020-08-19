aef479c3a7bf690fdf0473f070f11521d4d09b0c
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-18 21:58:11 -0300

    Estrutura de dados alteração2

--- Estrutura de dados/Busca_Binaria.py
+++ Estrutura de dados/Busca_Binaria.py
@@ -25,3 +25,31 @@ numeros = [5, 11 ,13 ,33 ,61, 73 ,79 ,80 ,85]
 print(busca_indice(numeros,5))
 print(busca_indice(numeros,85))
 print(busca_indice(numeros,7))
+
+print("-------------------------")
+
+def soma_lista(lista):
+    soma = 0
+    i = 0
+    while i < len(lista):
+        soma += lista[i]
+        i += 1
+    return soma
+
+print("-------------------------")
+
+def maior_linha(matriz):
+    n_linhas = len(matriz)
+    n_colunas = len(matriz[0])
+    linha_maior_soma = 0
+    i = 0
+    while i < n_linhas:
+        soma_atual = 0
+        j = 0
+        while j < n_colunas:
+            soma_atual += matriz[i][j]
+            j += 1
+        if soma_atual > linha_maior_soma or i == 0:
+            linha_maior_soma = soma_atual
+            i += 1
+    return linha_maior_soma
