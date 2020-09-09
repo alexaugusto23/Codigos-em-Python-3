b2664b1343a3de89d60124c9e045ec8198278816
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-08 21:22:08 -0300

    instanciamento

--- Estrutura de dados/POO_TAD/ponto.py
+++ Estrutura de dados/POO_TAD/ponto.py
@@ -4,3 +4,16 @@ class Ponto:
     def __init__ (self, x ,y):
         self.x = x
         self.y = y
+    
+    def obter_coordenadas(self):
+        return '(' + str(self.x) + ',' + str(self.y) + ')'
+    
+    def igual(self, p):
+        return (self.x == p.x) and (self.y == p.y)
+
+
+p1 = Ponto(0, 0)
+p2 = Ponto(0, 0)
+print( p1 == p2 ) # imprime True
+p3 = Ponto(2, 0)
+print ( p3 == p1 ) # imprime False
