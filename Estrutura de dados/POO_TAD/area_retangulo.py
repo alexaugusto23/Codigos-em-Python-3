4e344c0a4cf2b59831408c7b479416a8aeab7070
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-10 10:42:53 -0300

    novos arquivos

--- Estrutura de dados/POO_TAD/area_retangulo.py
+++ Estrutura de dados/POO_TAD/area_retangulo.py
@@ -1,26 +1,26 @@
 class Retangulo: 
 
-    def __init__ (self, comprimento, altura):
-        self.__comprimento = comprimento 
+    def __init__ (self, largura, altura):
+        self.__largura = largura 
         self.__altura = altura
 
     def area(self):
-        return self.__comprimento * self.__altura
+        return self.__largura * self.__altura
     
     def perimetro(self):
-        return (self.__comprimento * self.__altura) * 2
+        return (self.__largura * self.__altura) * 2
     
-    def get_comprimento(self):
-        return self.__comprimento
+    def get_largura(self):
+        return self.__largura
     
     def get_altura(self):
         return self.__altura
     
-    def set_comprimento(self, valor):
-            return self.__comprimento = valor
+    def set_largura(self, valor):
+        self.__largura = valor
     
     def set_altura(self, valor):
-        return self.__altura = valor
+        self.__altura = valor
 
 ret1 = Retangulo(5,10)
 print(ret1.area())
--- Estrutura de dados/POO_TAD/retangulo_questao2.py
+++ Estrutura de dados/POO_TAD/retangulo_questao2.py
@@ -1,7 +1,53 @@
 '''
-
+Questao 2: Implemente o TAD Retangulo:
+    B1 - Facil) Implemente os dados/atributos da classe: armazene as coordenadas
+    x e y do canto superior esquerdo, a largura e a altura, conforme a figura
+    abaixo.
+    B2 - Facil) Implemente o metodo area() que calcula a  ́area do retˆangulo.
+    B3 - Facil) Implemente o metodo perimetro() que calcula o perımetro (a soma
+    dos quatro lados) do retˆangulo.
+    B4 - Médio) Implemente o método interseccao() que recebe um parâmetro r
+    com outra instãncia da classe Retangulo e verifica se os retângulos se
+    intersectam.
 
 '''
 
 class Retangulo: 
-    pass
+
+    def __init__ (self, x =0 , y = 0, largura = 0,  altura = 0):
+        self.__largura = largura 
+        self.__altura = altura
+        self.__x = float(x)
+        self.__y = float(y) 
+
+    def area(self):
+        return self.__largura * self.__altura
+    
+    def perimetro(self):
+        return (self.__largura + self.__altura) * 2
+    
+    def interseccao(self, r):
+        if (self.__x == r.__x or self.__y == r.__y):
+            return f"\nRetangulo 1 está em intersecção com Retangulo 2: \nposição:|{self.__x},{self.__y}| |{r.__x},{r.__y}|"
+        else:
+            return f"\nOs retangulos não tem intersecção: \nposição:|{self.__x},{self.__y}| |{r.__x},{r.__y}|\n"
+    
+    def get_largura(self):
+        return self.__largura
+    
+    def get_altura(self):
+        return self.__altura
+    
+    def set_largura(self, valor):
+        self.__largura = valor
+    
+    def set_altura(self, valor):
+        self.__altura = valor
+
+cord_r_1 = Retangulo(0,1,5,10)
+cord_r_2 = Retangulo(1,2,5,10)
+
+#print(cord_r_1.area())
+#print(cord_r_1.perimetro())
+#print('endereco na memoria',cord_r_1)
+print(cord_r_1.interseccao(cord_r_2))
