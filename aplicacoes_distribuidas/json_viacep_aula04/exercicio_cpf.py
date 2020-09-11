5f2f76a8326d0a60eb589347109fa4b4437b94be
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-11 11:01:34 -0300

    inclusão de comentário de formação do cpf.

--- aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
+++ aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
@@ -15,3 +15,23 @@ pontuacao_input = str(input("Deseja pontuação ? S/N:")).upper()
 uf_input = str(input("Digite a UF do estado:")).upper()
 cpf = procura_cpf(pontuacao_input,uf_input)
 print(f"O CPF gerado foi {cpf}")
+
+'''
+Siginificado do cpf:
+
+EX: CPF XXX.XXX.XX"9" - YY
+
+0 - Rio Grande do Sul.
+1 - Distrito Federal, Goiás, Mato Grosso, 
+Mato Grosso do Sul e Tocantins.
+2 - Amazonas, Pará, Roraima, Amapá, Acré e 
+Rondônia.
+3 - Ceará, Maranhão e Piauí.
+4 - Paraíba, Pernambuco, Alagoas e Rio Grande do Norte.
+5 - Bahia e Sergipe.
+6 - Minas Gerais.
+7 - Rio de Janeiro e Espírito Santo.
+8 - São Paulo.
+9 - Paraná e Santa Catarina.
+
+'''
