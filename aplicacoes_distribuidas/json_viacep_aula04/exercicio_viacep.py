47518f60be91348ebe69b7ccdb3daace6889a821
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-10 23:38:00 -0300

    exercício viacep

---
+++ aplicacoes_distribuidas/json_viacep_aula04/exercicio_viacep.py
@@ -0,0 +1,16 @@
+import requests
+
+def procura_cep(cep):
+    a = requests.get('https://viacep.com.br/ws/'+cep+'/json/')
+    #dir(a)
+    dic = a.json()
+    return dic
+
+cep_input = str(input("Digite o cep: "))
+end = procura_cep(cep_input)
+#print(end,'\n')
+print('CEP:', end['cep'])
+print('LOGRADOURO:', end['logradouro']) 
+print('BAIRRO:', end['bairro']) 
+print('LOCALIDADE:', end['localidade']) 
+print('UF:', end['uf'])
