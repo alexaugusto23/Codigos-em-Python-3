960fd2585d44f607fe69c1d70d7a222be2c1dbe0
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-11 01:14:44 -0300

    correção no valor da string e dos parâmetros

--- aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
+++ aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
@@ -2,8 +2,8 @@ import requests
 ''
 def procura_cpf(pont,uf):
     dic_acoes = {"acao":"gerar_cpf","pontuacao":'S',"cpf_estado":"SP"}
-    #dic_acoes['pontuacao'] = pont
-    #dic_acoes['cpf_estado'] = uf
+    dic_acoes['pontuacao'] = pont
+    dic_acoes['cpf_estado'] = uf
     requisicao = requests.post('https://www.4devs.com.br/ferramentas_online.php',dic_acoes)
     print (f'\nURL:{requisicao.url}\nStatus:{requisicao.status_code}\nRequest:{requisicao.request}\n')   
     #print(dir(requisicao))
@@ -13,5 +13,5 @@ def procura_cpf(pont,uf):
 
 pontuacao_input = str(input("Deseja pontuação ? S/N:")).upper()
 uf_input = str(input("Digite a UF do estado:")).upper()
-cpf = procura_cpf(uf_input,pontuacao_input)
+cpf = procura_cpf(pontuacao_input,uf_input)
 print(f"O CPF gerado foi {cpf}")
