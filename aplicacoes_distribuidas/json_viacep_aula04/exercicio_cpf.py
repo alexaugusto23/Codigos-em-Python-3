f114f35b5ac21150fa1cf2efa00010d2159b7c79
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-11 01:05:14 -0300

    criação de dict

--- aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
+++ aplicacoes_distribuidas/json_viacep_aula04/exercicio_cpf.py
@@ -1,17 +1,17 @@
 import requests
 ''
-def procura_cpf(UF,pontuacao):
-    pontuacao.upper()
-    requisicao = requests.post('https://www.4devs.com.br/ferramentas_online.php',{"acao":"gerar_cpf","pontuacao":"+pontuacao+","cpf_estado":"+UF+"})
+def procura_cpf(pont,uf):
+    dic_acoes = {"acao":"gerar_cpf","pontuacao":'S',"cpf_estado":"SP"}
+    #dic_acoes['pontuacao'] = pont
+    #dic_acoes['cpf_estado'] = uf
+    requisicao = requests.post('https://www.4devs.com.br/ferramentas_online.php',dic_acoes)
     print (f'\nURL:{requisicao.url}\nStatus:{requisicao.status_code}\nRequest:{requisicao.request}\n')   
     #print(dir(requisicao))
     resposta = requisicao.text
     #print("\n",resposta)
     return resposta
 
-#procura_cpf()
-
-'''
-uf_input = str(input("Digite a UF do estado: "))
- = procura_cep(uf_input)
-#print(end,'\n')
+pontuacao_input = str(input("Deseja pontuação ? S/N:")).upper()
+uf_input = str(input("Digite a UF do estado:")).upper()
+cpf = procura_cpf(uf_input,pontuacao_input)
+print(f"O CPF gerado foi {cpf}")
