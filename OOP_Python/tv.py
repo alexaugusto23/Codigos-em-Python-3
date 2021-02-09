167172d4deb8500ccf30afcf50630ef4c1437ac3
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-08 22:44:17 -0300

    melhorias no módulos da classe

--- OOP_Python/tv.py
+++ OOP_Python/tv.py
@@ -3,9 +3,21 @@ class Televisao():
         self.canal = 0
         self.volume = 0
         self.status =  False
+        self.msg = "Televisão desligada"
 
-    def status_on_off(self, valor):
-        self.status = valor
+    def troca_status(self):
+        print("A Televisão está: ")
+        self.status_on_off()
+        status = input("Ligar ou Desligar Televisão: ")
+                
+        if (status.lower() == "ligar"):
+            self.status = True
+            self.status_on_off()
+        elif (status.lower() == "desligar"):
+            self.status = False
+            self.status_on_off()
+        else:
+            print("Esse modo não existe!!!")
 
     
     def status_on_off(self):
@@ -13,26 +25,53 @@ class Televisao():
             print('Ligada!!!')
         else:
             print('Desligada!!!')
+    
 
     def trocar_canal(self, novo_canal):
-        troca = self.canal = novo_canal
-        return troca
+        if (self.status == True):
+            troca = self.canal = novo_canal
+            print(f"Canal trocado: {self.canal}")
+        else:
+            print(self.msg)
     
     def troca_volume(self, novo_volume):
-        troca = self.volume = novo_volume
-        return troca
+        if (self.status == True):
+            troca = self.volume = novo_volume
+            print(f"Volume trocado: {self.volume}")
+        else:
+            print(self.msg)
+
     
     def aumenta_canal(self):
-        self.canal += 1
+        if (self.status == True):
+            self.canal += 1
+            print(f"Canal mudado para: {self.canal}")
+        else:
+            print(self.msg)
+        
     
     def diminui_canal(self):
-        self.canal -= 1
+        if (self.status == True):
+            self.canal -= 1
+            print(f"Canal mudado para: {self.canal}")
+        else:
+            print(self.msg)
     
     def aumenta_volume(self):
-        self.canal += 1
+        if (self.status == True):
+            self.volume += 1
+            print(f"Volume diminuído para: {self.volume}")
+        else:
+            print(self.msg)
     
     def diminui_volume(self):
-        self.canal -= 1
+        if (self.status == True):
+            self.volume -= 1
+            print(f"Volume diminuído para: {self.volume}")
+        else:
+            print(self.msg)
 
 tv = Televisao()
-tv.status_on_off()
+tv.troca_status()
+tv.aumenta_canal()
+tv.aumenta_canal()
