d3fc065ef4d4e38beaf6e0733b5ff7b2a60598f8
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-08 21:42:59 -0300

    criando métodos da tv

--- OOP_Python/tv.py
+++ OOP_Python/tv.py
@@ -1,19 +1,38 @@
-class Tv:
-    def __init__(self, canal, volume):
-        self.canal = canal
-        self.volume = volume
+class Televisao():
+    def __init__(self):
+        self.canal = 0
+        self.volume = 0
         self.status =  False
+
+    def status_on_off(self, valor):
+        self.status = valor
+
     
     def status_on_off(self):
         if self.status == True:
-            print('Ligado!!!')
+            print('Ligada!!!')
         else:
-            print
+            print('Desligada!!!')
 
     def trocar_canal(self, novo_canal):
         troca = self.canal = novo_canal
         return troca
     
-    def trocar_volume(self, novo_volume):
+    def troca_volume(self, novo_volume):
         troca = self.volume = novo_volume
         return troca
+    
+    def aumenta_canal(self):
+        self.canal += 1
+    
+    def diminui_canal(self):
+        self.canal -= 1
+    
+    def aumenta_volume(self):
+        self.canal += 1
+    
+    def diminui_volume(self):
+        self.canal -= 1
+
+tv = Televisao()
+tv.status_on_off()
