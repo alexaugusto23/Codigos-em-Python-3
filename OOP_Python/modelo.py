4266f53a7f55a9549308de1960a7e3cc6a171c98
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-10 22:08:40 -0300

    melhorando o polimorfismo daclasse.

--- OOP_Python/modelo.py
+++ OOP_Python/modelo.py
@@ -19,25 +19,41 @@ class Programa:
     
     def dar_like(self):
         self.__likes += 1
+    
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.likes}"
+
         
  
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self):
+        str = "minutos"
+        return f"{self.nome} - {self.ano} - {self.likes} - {self.duracao} {str}"
                  
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
     
+    def __str__(self):
+        str = "temporada" if self.temporadas <= 1 else "temporadas"
+        return f"{self.nome} - {self.ano} - {self.likes} - {self.temporadas} {str}"
+    
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+et = Filme('et', 1990, 140)
 atlanta = Serie('atlanta', 2018, 2)
+secret_key = Serie('secret key', 2017, 1)
+filmes_e_series = [vingadores, et, atlanta, secret_key]
 
 atlanta.dar_like()
 atlanta.dar_like()
 vingadores.dar_like()
 
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração0: {vingadores.duracao} - Likes {vingadores.likes}")
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes {atlanta.likes}")
+
+for programa in filmes_e_series:
+    print(programa)
